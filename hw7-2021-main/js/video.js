5a3f91b2fe9fc2b1407235f214bf8e2adebd50a6
JulyDing <143539244+JulyDing@users.noreply.github.com>
2024-04-07 21:34:21 -0400

    Add files via upload

---
+++ hw7-2021-main/js/video.js
@@ -0,0 +1,84 @@
+var video;
+
+window.addEventListener("load", function() {
+	console.log("Good job opening the window")
+	video = document.querySelector("#player1");
+	video.autoplay=false;
+	video.loop=false;
+	console.log("auto play is set to" + video.autoplay);
+	console.log("loop is set to" + video.loop);
+});
+
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+	video.play();
+	document.querySelector("#volume").innerHTML = video.volume * 100 + "%";
+}
+);
+
+document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video");
+	video.pause();
+}
+);
+
+document.querySelector("#slower").addEventListener("click", function() {
+	console.log("Slow Down Video");
+	video.playbackRate *= 0.95;
+	console.log("New speed is " + video.playbackRate);
+}
+);
+
+document.querySelector("#faster").addEventListener("click", function() {
+	console.log("Speed Up Video");
+	video.playbackRate /= 0.95;
+	console.log("New speed is " + video.playbackRate);
+}
+);
+
+document.querySelector("#skip").addEventListener("click", function() {
+	console.log("Skip Ahead");
+	if (video.currentTime + 15 > video.duration) {
+		video.currentTime = 0;
+	}
+	else {
+		video.currentTime += 15;
+	}
+	console.log("New time is " + video.currentTime);
+}
+);
+
+document.querySelector("#mute").addEventListener("click", function() {
+	console.log("Mute Video");
+	if (video.muted == false) {
+		video.muted = true;
+		document.querySelector("#mute").innerHTML = "Unmute";
+	}
+	else {
+		video.muted = false;
+		document.querySelector("#mute").innerHTML = "Mute";
+	}
+}
+);
+
+document.querySelector("#slider").addEventListener("change", function() {
+	console.log("Change Volume");
+	console.log(this.value);
+	video.volume = this.value / 100;
+	document.querySelector("#volume").innerHTML = video.volume * 100 + "%";
+}
+);
+
+document.querySelector("#vintage").addEventListener("click", function() {
+	console.log("Change CSS");
+	video.classList.add("oldSchool");
+}
+);
+
+document.querySelector("#orig").addEventListener("click", function() {
+	console.log("Change CSS");
+	video.classList.remove("oldSchool");
+}
+);
+
+
